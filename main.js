d602c92449bba116235a24f9a2d1e9b48ecaad59
hooverlds2020 <hooverlds93@gmail.com>
2022-11-20 10:23:11 -0600

    caurto  commit

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-const input = document.getElementById('cost')
-console.log(input)
-input.textContent = Hola
+const pago = document.getElementById('pago').value
+console.log(pago)
+const person = document.getElementById('person').value
+console.log(person)
+const btns = document.querySelectorAll('button.btns')
+console.log(btns)
+
+let personAmount = document.querySelector('p.personAmount')
+console.log(personAmount)
+
+btns.forEach(e => {
+    e.addEventListener('click', e => {
+        let infBtns = parseInt(e.target.innerText.slice(0, -1))
+        console.log(infBtns)
+    })
+})
